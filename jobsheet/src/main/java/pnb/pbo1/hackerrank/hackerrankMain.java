5c0bae9a4332995a67aea460da079f4d29d0795c
Prajna Paramahansa <91102339+SparkyCloudy@users.noreply.github.com>
2023-05-08 00:05:39 +0800

    Delete jobsheet directory

--- jobsheet/src/main/java/pnb/pbo1/hackerrank/hackerrankMain.java
+++
@@ -1,7 +0,0 @@
-package pnb.pbo1.hackerrank;
-
-public class hackerrankMain {
-	public hackerrankMain() {
-		new pnb.pbo1.hackerrank.SubstringComparison.Solution();
-	}
-}
--- jobsheet/src/main/java/pnb/pbo1/js/helper/Animal.java
+++
@@ -1,7 +0,0 @@
-package pnb.pbo1.js.helper;
-
-public class Animal {
-	public void move() {
-		System.out.println("I'am running!");
-	}
-}
